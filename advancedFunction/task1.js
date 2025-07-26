8968fa1a80190aa9d3707cd6f475d28d7e200b64
Nataliia Akoieva <natalia.akoieva@gmail.com>
2025-07-26 14:10:36 +0300

    7.1. Advanced function

---
+++ advancedFunction/task1.js
@@ -0,0 +1,18 @@
+function handleNum(num, onEvenCb, onOddCb) {
+    if (num % 2 === 0) {
+        onEvenCb();
+    } else {
+        onOddCb();
+    }
+}
+
+function handleEven() {
+    console.log(`${num} is even`);
+}
+
+function handleOdd() {
+    console.log(`${num} is odd`);
+}
+
+let num = Date.now();
+handleNum(num, handleEven, handleOdd);
